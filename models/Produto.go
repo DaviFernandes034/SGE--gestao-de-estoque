017f21715c8eedd70d298304639aeb06d2971e9b
davi <davifernadesr@gmail.com>
2024-11-26 02:38:40 -0300

    mudando o atributo lote para string

--- models/Produto.go
+++ models/Produto.go
@@ -6,11 +6,11 @@ import "time"
 type Produtos struct{
 
 	Id_produto int `json:"produto_id"`
-	nome string `json:"nome"`
-	preco float64 `json:"preco"`
-	lote int `json:"lote"`
-	validade time.Time `json:validade`
-	categoria Categorias `json:"categoria"`
+	Nome string `json:"nome"`
+	Preco float64 `json:"preco"`
+	Lote string `json:"lote"`
+	Validade time.Time `json:validade`
+	CategoriaId int64 `json:categoriaId`
 	
 
 }
