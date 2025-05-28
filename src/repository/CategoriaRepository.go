15b47a9b346e9a21c27f9d81bfab6d3b7b881d03
davi <davifernadesr@gmail.com>
2025-05-27 21:25:17 -0300

    uma dos repositorys (provavelmente vai virar interface)

---
+++ src/repository/CategoriaRepository.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	
+	"log"
+	"github.com/DaviFernandes034/SGE--gestao-de-estoque/configs"
+)
+
+type Repository struct {
+	db *configs.Connection
+}
+
+func NewRepository(db *configs.Connection)*Repository{
+
+	log.Println("INFO: repository conectado com o banco de dados")
+	return &Repository{
+		db: db,
+	}
+
+
+}
