8274015ccebadfead5c1b27880c9a34f4c35d278
davi <davifernadesr@gmail.com>
2025-05-28 20:00:21 -0300

    interface para metados genericos do service

---
+++ src/interfaces/interfaceService.go
@@ -0,0 +1,13 @@
+package interfaces
+
+//interface criada para não ficar digitando as mesmas coisas
+//aqui tem todos os metados CRUD(create,read, update, delete)
+//usado em todo Service
+type ServiceCrud[t any] interface {
+/*  esse T any, é um tipo generico, aonde posso passar todos os models como parametro*/
+	Save(entity t) 
+	SearchID(id int) 
+	Update(entity t) 
+	Delete(entity t) 
+	SearchAll()
+}
