473b67852c32bc7adf80965879059fd94a06f0bf
davi <davifernadesr@gmail.com>
2025-05-28 20:10:21 -0300

    uml

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	
+
 	"log"
 	"net/http"
 
@@ -11,13 +11,16 @@ import (
 
 func main(){
 
-	
+	log.Println("SGE-SISTEMA DE GESTAO DE ESTOQUE!")
+	log.Print("desenvolvedor: Davi fernandes")
 	//carregando os arquivos .env
 	err:= godotenv.Load()
 	if err != nil {
 
 		log.Fatalf("erro ao carregar o arquivo .env: %v", err)
 	}
+
+	log.Println("ARQUIVOS .ENV CARREGADOS")
 
 	var InitConnection configs.InitConnection //iniciando a inicializaçao do banco de dados
 
@@ -27,9 +30,11 @@ func main(){
 		log.Fatalf("erro ao iniciar conexão: %v", err)
 	}
 
-	
+	log.Println("INFO: CONEXÃO COM O BANCO DE DADOS, COMPLETA!")
 
 	log.Println("INFO: aplicação iniciada com sucesso!!")
+	log.Println("----------------------------------------------------------------")
+
 	http.ListenAndServe(":8080", nil)
 	
 }
