d56f2c2adbde75215d261d06fa8dc17f7fd5e6f5
davi <davifernadesr@gmail.com>
2025-05-27 21:24:50 -0300

    criação de testes

--- src/main.go
+++ src/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	
 	"log"
 	"net/http"
 
 	"github.com/DaviFernandes034/SGE--gestao-de-estoque/configs"
+	"github.com/joho/godotenv"
 )
 
 func main(){
 
-	var InitConnection configs.InitConnection
-
-	err:= InitConnection.Init()
+	
+	//carregando os arquivos .env
+	err:= godotenv.Load()
 	if err != nil {
 
-		log.Fatalf("erro ao iniciar conexão: %w", err)
+		log.Fatalf("erro ao carregar o arquivo .env: %v", err)
+	}
+
+	var InitConnection configs.InitConnection //iniciando a inicializaçao do banco de dados
+
+	db,err := InitConnection.Init() //chamando a funcao para iniciar a conexão
+	if err != nil {
+
+		log.Fatalf("erro ao iniciar conexão: %v", err)
 	}
 
 	
